c7d0e8e8d9d7d888d3ccb73d5e623e81ef000554
Oren Cohen <oren.c90@gmail.com>
2020-01-02 23:24:20 +0200

    cleaning my code

--- dlFromList.py
+++ dlFromList.py
@@ -3,22 +3,6 @@ import youtube_dl
 import os
 from sys import argv
 
-# class MyLogger(object):
-#     def debug(self, msg):
-#         pass
-#
-#     def warning(self, msg):
-#         pass
-#
-#     def error(self, msg):
-#         print(msg)
-
-
-# def my_hook(d):
-#     if d['status'] == 'finished':
-#         print('Done downloading, now converting ...')
-
-
 ydl_opts = {
     'format': 'bestaudio/best',
     'outtml': '%(title)s.%(ext)s',
