728577deb215ea13e15a8306e7b093ae5db2b8a3
Oren Cohen <oren.c90@gmail.com>
2020-01-07 00:37:49 +0200

    using YOUTUBE DATA API for search

--- dlFromList.py
+++ dlFromList.py
@@ -2,27 +2,79 @@ from __future__ import unicode_literals
 import youtube_dl
 import os
 from sys import argv
+import requests
+import webbrowser
+import settings
+import errno
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtml': '%(title)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    # 'logger': MyLogger(),
-    # 'progress_hooks': [my_hook],
-}
+# globals:
 
-if not os.path.exists('Songs'):
-    os.mkdir('Songs')
-os.chdir('Songs')
+url_search = 'https://www.googleapis.com/youtube/v3/search'
+url_watch = 'https://www.youtube.com/watch?v='
+cur_dir = os.getcwd()
 
-if len(argv) < 1:
-    raise IndexError('please enter file name')
 
-with open('../' + argv[1], 'r') as f:
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        for song_url in f:
-            ydl.download([song_url])
+def get_url_id(title):
+    global url_search
+    params = {
+        'part': 'snippet',
+        'q': title,
+        'key': settings.KEY,
+        'maxResults': 1,
+        'type': 'video'
+    }
+    res = requests.get(url_search, params=params)
+    url_id = res.json()['items'][0]['id']['videoId']
+    try:
+        title = res.json()['items'][0]['snippet']['title']
+        if 'official' not in title.lower():
+            print('not official ' + title)
+    except:
+        print('no title')
+    return url_id
+
+
+def main():
+    song_list, folder = input_validation()
+
+    # youtube-dl options download
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(folder, '%(title)s.%(ext)s'),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with open(song_list, 'r') as f:  # open songs list file
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            for song_title in f:
+                song_title = song_title.strip()
+                song_url = get_url_id(song_title)
+                ydl.download([song_url])
+
+
+def input_validation():
+    if len(argv) == 3:
+        songs_list = argv[1]
+        folder = argv[2]
+    elif len(argv) < 2:
+        # raise IndexError('please enter file name')
+        songs_list = input('please chose a song list (with extension)')
+        folder = input('please specify an output folder for downloading')
+    else:
+        raise IndexError('this scripts get 0 or 2 arguments!')
+
+    if not os.path.isfile(songs_list):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), songs_list)
+
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+
+    return songs_list, folder
+
+
+if __name__ == '__main__':
+    main()
